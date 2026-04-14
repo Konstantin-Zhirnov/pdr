10221df5ca262bd7b694e67abdf873e9ac28bd4d
Konstantin Zhirnov <kostya.zhirnov@gmail.com>
2026-04-13 21:21:15 -0700

    email

--- app/components/Email/index.tsx
+++ app/components/Email/index.tsx
@@ -5,7 +5,7 @@ import classes from './Email.module.sass'
 const Email: React.FC = () => {
   return (
     <a
-      href="mailto:westpointwest1979@gmail.com?subject=Paintless%20Dent%20Removal"
+      href="mailto:pushdentback@icloud.com?subject=Paintless%20Dent%20Removal"
       className={classes.link}
     >
       Send Email
